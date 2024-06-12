c965cf4bebea073042cc1460d297edea46997a50
Nelly-10 <nelson_nonso@yahoo.com>
2024-06-12 06:34:01 +0100

    first commit

---
+++ src/components/recipe-list/index.jsx
@@ -0,0 +1,7 @@
+
+
+export default function RecipeItem({recipe}) {
+    return <div>
+        Recipe Item...
+    </div>
+}
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,23 +4,33 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
   const [searchParams, setSearchParams] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
 
   async function handleSubmit(event) {
     event.preventDefault();
     try {
+      setLoading(true);
       const res = await fetch(
-        `https://forkify-api.herokuapp.com/api/get?${searchParams}`
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParams}`
       );
       const data = await res.json();
+      if (data?.data?.recipe) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParams("");
+      }
       console.log(data);
     } catch (e) {
       console.log(e);
+      setLoading(false);
+      setSearchParams("");
     }
   }
 
   return (
     <GlobalContext.Provider
-      value={{ searchParams, setSearchParams, handleSubmit }}
+      value={{ searchParams, setSearchParams, handleSubmit, loading, recipeList }}
     >
       {children}
     </GlobalContext.Provider>
